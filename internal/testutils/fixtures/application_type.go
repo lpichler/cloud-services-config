612ad155fa48ffcdf1cbffc55fd51fa1ea7cd66c
Libor Pichler <lpichler@redhat.com>
2022-06-10 11:09:08 +0200

    Test create users in bulk create

--- internal/testutils/fixtures/application_type.go
+++ internal/testutils/fixtures/application_type.go
@@ -1,6 +1,10 @@
 package fixtures
 
-import m "github.com/RedHatInsights/sources-api-go/model"
+import (
+	m "github.com/RedHatInsights/sources-api-go/model"
+)
+
+var userOwnership = "user"
 
 var TestApplicationTypeData = []m.ApplicationType{
 	{
@@ -12,5 +16,18 @@ var TestApplicationTypeData = []m.ApplicationType{
 		Id:          2,
 		Name:        "second app type name",
 		DisplayName: "second test app type",
+	},
+	{
+		Id:                   3,
+		DisplayName:          "app-studio",
+		Name:                 "/insights/platform/app-studio",
+		ResourceOwnership:    &userOwnership,
+		SupportedSourceTypes: []byte(`["bitbucket", "dockerhub", "github", "gitlab", "quay"]`),
+	},
+	{
+		Id:                   4,
+		DisplayName:          "Cost Management",
+		Name:                 "/insights/platform/cost-management",
+		SupportedSourceTypes: []byte(`["amazon", "azure", "google", "oracle-cloud-infrastructure", "openshift", "ibm"]`),
 	},
 }
